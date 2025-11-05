d589a54aa8d8143f4be2b52bf384ec03964c1252
neyugnyt <sakute2k@gmail.com>
2025-11-05 16:18:49 +0700

    change setup

--- BE/BE/Startup.cs
+++ BE/BE/Startup.cs
@@ -84,7 +84,7 @@ namespace BE
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseCors("AllowAll");
+            app.UseCors("AllowProduction");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
